24982488b09ce8d3639a9301c6efb969b4f02050
sanbo1 <sanbo.dev@gmail.com>
2016-08-19 09:47:08 +0900

    repair get_temp.sh

--- thermometer_ds18b20/get_temp.sh
+++ thermometer_ds18b20/get_temp.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
 DEVID="28-021563e37cff"
 
-cat /sys/bus/w1/devices/$DEVID/w1_slave \
-	     | perl -e 'while(<stdin>){ if(/t=([-0-9]+)/){print $1/1000,"\n";} }'
+NOW=$(echo $(date +%Y/%m/%d-%H:%M:%S) | sed -e "s/[\r\n]\+//g")
+
+cat /sys/bus/w1/devices/$DEVID/w1_slave | while read line
+do
+if [ $(echo "$line" | grep -c "t=") -eq 1 ]; then
+	TEMP=$(echo "$line" | grep -e "t=" | sed -e 's/^.*t=\([0-9]*\).*$/\1/')
+	echo "[${NOW}] temp=`expr ${TEMP} / 1000`"
+fi
+done
+
