2f9930ec574eef4f0f687a35bb5852efe0db58f4
sanbo1 <sanbo.dev@gmail.com>
2016-08-28 22:11:56 +0900

    add express

--- thermometer_madt7410/index.js
+++ thermometer_madt7410/index.js
@@ -23,3 +23,17 @@ setInterval(function() {
 	readValue();
 },INTERVAL);
 
+
+// Express
+var express = require('express');
+var app = express();
+
+app.use(express.static('htdocs'));
+
+var port = 3000;
+
+app.listen(port, function(){
+	console.log("Express サーバがポート%dで起動しました。モード:%s"
+		, port, app.settings.env);
+});
+
